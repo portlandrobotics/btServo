fcb7cefc5720e02ee7b598f7a5c4106b8ccc94b0
Jeff Miller <git@jmd5.com>
2014-05-12 17:08:07 -0700

    Added ADC read code, and a basic PWM function.  Replaced main loop to call them.

--- firmware/main.c
+++ firmware/main.c
@@ -64,14 +64,96 @@ void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p
  **/
 void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)                        
 {
-  app_error_handler(0xdeadbeefF, line_num, p_file_name);
+  app_error_handler(0xdeadbeef, line_num, p_file_name);
+}
+
+/* ADC Setup routine stolen from battery example */
+void adc_setup(void)
+{
+  // P0.02 is AIN3
+
+  NRF_ADC->CONFIG = (ADC_CONFIG_RES_8bit        << ADC_CONFIG_RES_Pos)     |
+    (ADC_CONFIG_INPSEL_AnalogInputOneThirdPrescaling    << ADC_CONFIG_INPSEL_Pos)  |
+    (ADC_CONFIG_REFSEL_VBG                      << ADC_CONFIG_REFSEL_Pos)  |
+    (ADC_CONFIG_PSEL_AnalogInput3                       << ADC_CONFIG_PSEL_Pos)    |
+    (ADC_CONFIG_EXTREFSEL_None                  << ADC_CONFIG_EXTREFSEL_Pos);
+  //NRF_ADC->EVENTS_END = 0;
+  NRF_ADC->ENABLE     = ADC_ENABLE_ENABLE_Enabled;
+
+
+  //NRF_ADC->EVENTS_END  = 0;    // Stop any running conversions.
+  //NRF_ADC->TASKS_START = 1;
+
+}
+
+uint8_t adc_read(void)
+{
+    NRF_ADC->TASKS_START = 1;
+    while(NRF_ADC->BUSY)
+	;
+    uint8_t rv = NRF_ADC->RESULT;
+    return rv;
+}
+
+void motor_update(float mdrive)
+{
+    float absmdrive = mdrive;
+    if(mdrive > 0.)
+    {
+	nrf_gpio_pin_clear(12);
+    }
+    else
+    {
+	nrf_gpio_pin_set(12);
+	absmdrive = -mdrive;
+    }
+
+    uint8_t pwm = 0;
+    if(absmdrive >= 1.)
+	pwm = 0xff;
+    else
+	pwm = 0xff*absmdrive;
+    nrf_pwm_set_value(0,pwm);
 }
 
 
+// pid code stolen from http://www.ece.ucdavis.edu/~spencer/195/datasheets/PID-Without-a-PhD.pdf
+typedef struct
+{
+    float dState; // Last position input
+    float iState; // Integrator state
+    float iMax, iMin;
+    // Maximum and minimum allowable integrator state
+    float iGain, // integral gain
+	pGain, // proportional gain
+	dGain; // derivative gain
+} SPid;
+
+float UpdatePID(SPid * pid, float error, float position)
+{
+    float pTerm, dTerm, iTerm;
+
+    // calculate the proportional term
+    pTerm = pid->pGain * error;
+
+    // calculate the integral state with appropriate limiting
+    pid->iState += error;
+    if (pid->iState > pid->iMax)
+	pid->iState = pid->iMax;
+    else if (pid->iState < pid->iMin)
+	pid->iState = pid->iMin;
+    iTerm = pid->iGain * pid->iState; // calculate the integral term
+
+    dTerm = pid->dGain * (position - pid->dState);
+    pid->dState = position;
+
+    return pTerm + iTerm - dTerm;
+}
+#if 0
 const uint8_t sin_table[] = {0, 0,1,2,4,6,9,12,16,20,24,29,35,40,	46,	53,	59,	66,	74,	81,	88,	96,	104,112,120,128,136,144,152,160,168,175,182,190,197,203,210,216,221,227,
   232,236,240,244,247,250,252,254,255,255,255,255,255,254,252,250,247,244,240,236,232,227,221,216,210,203,197,190,182,175,168,160,152,144,136,128,120,112,104,
   96,88,81,74,66,59,	53,	46,	40,	35,	29,24,	20,	16,	12,	9,	6,	4,	2,1,0};
-
+#endif
 int main(void)
 {
   uint32_t counter = 0;
@@ -94,9 +176,50 @@ int main(void)
   // Initialize the PWM library
   nrf_pwm_init(&pwm_config);
 
-  int direction = -1;
+  //int direction = -1;
   nrf_gpio_pin_clear(12);
+  nrf_pwm_set_value(0, 0x80);
 
+  adc_setup();
+
+  SPid pidstate = {0,0, 0,0, 0,0,0};
+
+  pidstate.pGain = 2;
+  //pidstate.iGain = 0.01;
+  //pidstate.dGain = 10*z;
+
+  pidstate.iMax = 1/pidstate.iGain;
+  pidstate.iMin = -pidstate.iMax;
+
+  float ts[] = {.25,.75};
+  int i=0;
+
+
+  float ftarget = .5;
+
+  // the stop is at ~3.22v
+  // vref is 1.2V, so top of range is 3.22V / 3 / 1.2 * 256 = 229
+
+  while(1)
+  {
+      uint8_t adcval = adc_read();
+      float pos = (float)adcval / 229.;
+      //float cmd = UpdatePID(&pidstate,pos-ftarget,pos);
+      motor_update(pos-ftarget);
+      nrf_delay_us(8000);
+      counter++;
+      if(counter >= 400)
+      {
+	  i = (i+1)%2;
+	  ftarget = ts[i];
+	  counter = 0;
+
+	  pidstate.iState=0;
+	  pidstate.dState=pos;
+      }
+  }
+
+#if 0
   while (true)
   {
     if (counter == 0) {
@@ -120,4 +243,5 @@ int main(void)
     // Add a delay to control the speed of the sine wave
     nrf_delay_us(8000);
   }
+#endif
 }
